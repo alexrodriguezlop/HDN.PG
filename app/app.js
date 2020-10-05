6577fe7ff70b2125e1345ad15c59c1a7921e5b9c
alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-10-05 18:12:24 +0200

    Corrección de estructura

--- app/app.js
+++ app/app.js
@@ -1,9 +1,18 @@
-var express = require('express')
-var app = express();
-var port = process.env.PORT || 3000;
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
 
-var routes = require('routes');
-routes(app);
-app.listen(port,function(){
-console.log('Server started on port: ' + port);
-});
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
