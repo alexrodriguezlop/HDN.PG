3cdade72b788d5b21f032ea8b367db4817c2a2c1
alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-10-07 15:51:00 +0200

    Actualizadas las clases

--- src/class/imagen.js
+++ src/class/imagen.js
@@ -2,5 +2,29 @@
  * Clase objeto imagen 
  * */
 class imagen {
+	constructor()
+	{
+
+	}
+
+	/**
+	Carga una imagen desde un fichero 
+	Parámetro A nombre del fichero que contiene la imagen
+	Devolverá true 	si ha tenido éxito en la lectura o false en caso contrario.
+	*/
+	leerImagen(A){
+
+	}
+
+
+	/**
+	Guarda una imagen desde un fichero 
+	Parámetro A nombre del fichero que contiene la imagen
+	Devolverá true 	si ha tenido éxito en la escritura o false en caso contrario.
+	*/
+	escribirImagen(A){
+	
+	} 
+	
 
 }
