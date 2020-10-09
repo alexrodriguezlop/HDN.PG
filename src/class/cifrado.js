38667793787ffcd44f6c05629d72aa3b34e5ba5f
Alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-10-09 20:59:04 +0200

    Editado fichero cifrado.js

--- src/class/cifrado.js
+++ src/class/cifrado.js
@@ -1,34 +1,37 @@
 const Imagen = require('./imagen.js')
 
 /**
- * Clase encargada de realizar operaciones de cifrado sobre una imágen 
+ * Módulo encargado de realizar operaciones de cifrado sobre una imágen 
  * */
-class cifrado {
-
-	/**
-	Oculta una cadena dentro de una imagen 
-	Parámetro A cadena que contendra el mensaje a ocultar
-	Devolverá el estado de la ejecución de la operación a modo de bool
-
-	La cadena debera tener un tamaño adecuado, en caso de 
-	no caber en la imagen se producirá un error.
-	*/
-	ocultar(A){
-
-	}
 
 
+/**
+Oculta una cadena dentro de una imagen 
+Parámetro A cadena que contendra el mensaje a ocultar
+Devolverá el estado de la ejecución de la operación a modo de bool
 
+La cadena debera tener un tamaño adecuado, en caso de 
+no caber en la imagen se producirá un error.
 
-	/**
-	Recupera una cadena oculta en una imagen 
-	Parámetro A Cadena donde se almacenará el mensaje
-
-	Devolverá la cadena oculta en la imagen.
-	Existe el caso de error en que la imagen no contenga mensaje oculto
-	*/
-	revelar(A){
-
-	}
+HU-1 Ocultar mensaje
+*/
+ocultar(A){		
 
 }
+
+
+
+
+/**
+Recupera una cadena oculta en una imagen 
+Parámetro A Cadena donde se almacenará el mensaje
+
+Devolverá la cadena oculta en la imagen.
+Existe el caso de error en que la imagen no contenga mensaje oculto
+
+HU-3 Revelar mensaje oculto
+*/
+revelar(A){
+
+}
+
