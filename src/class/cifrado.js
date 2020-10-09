1f2540918a425e3c0d28afae7891affbf63c3113
Alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-10-09 21:13:43 +0200

    Corregido fichero cifrado.js

--- src/class/cifrado.js
+++ src/class/cifrado.js
@@ -1,4 +1,4 @@
-const Imagen = require('./imagen.js')
+var Imagen = require('./imagen.js')
 
 /**
  * Módulo encargado de realizar operaciones de cifrado sobre una imágen 
@@ -8,6 +8,7 @@ const Imagen = require('./imagen.js')
 /**
 Oculta una cadena dentro de una imagen 
 Parámetro A cadena que contendra el mensaje a ocultar
+Parámetro B Imagen destino
 Devolverá el estado de la ejecución de la operación a modo de bool
 
 La cadena debera tener un tamaño adecuado, en caso de 
@@ -15,25 +16,25 @@ no caber en la imagen se producirá un error.
 
 HU-1 Ocultar mensaje
 */
-function ocultar(A){		
+function ocultar(A, B){		
 
 }
 
 /**
 Comprueba si existe una cadena dentro de una imagen 
 Devolverá el estado de la ejecución de la operación a modo de bool
-
+Parámetro A Imagen fuente
 
 HU-2 Comprobar si existe mensaje
 */
-function chequear(){		
+function chequear(A){		
 
 }
 
 
 /**
 Recupera una cadena oculta en una imagen 
-Parámetro A Cadena donde se almacenará el mensaje
+Parámetro A Imagen fuente
 
 Devolverá la cadena oculta en la imagen.
 Existe el caso de error en que la imagen no contenga mensaje oculto
@@ -49,6 +50,8 @@ Recupera los metadatos de cifrado en una imagen
 
 Devolverá una cadena con los metadatos de la imagen.
 Existe el caso de error en que la imagen no contenga metadatos.
+
+Parámetro A Imagen fuente
 
 HU-4 Conocer fecha y hora del cifrado
 */
