b816b4d494d0303c85b0ee93588204c356b92f7b
Alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-10-19 21:54:48 +0200

    Corrigiendo test #32

--- test/test.js
+++ test/test.js
@@ -1,14 +1,19 @@
-const { strictEqual } = require('assert');
 const Imagen = require('../src/class/imagen')
-var assert = require('assert').strict;
+var assert = require('chai').assert;
+
 
 describe("IMAGEN CLASS TEST", function() { 
-  let imagen = new Imagen();
+  let imagen = new Imagen(3,3);
 
-  it("Debería crear una imagen de 0x0 ", function() {
-     
-      assert.notStrictEqual(imagen.columnas, 0);
-      assert.notStrictEqual(imagen.filas, 0);
+  it("Debería crear una imagen de 3x3 con sus valores de pixel a 0", function() {
+    assert.notEqual(3, 4, 'these numbers are not equal');
+
+    assert.equal(imagen.getFilas, 3, "Debería ser 3");
+    assert.equal(imagen.getColumnas, 3, "Debería ser 3");
+
+      for (var i = 0; i < 9; i++) {
+        assert.equal(imagen.getPixel(i), 0, "Píxel distinto de 0");
+      }
   });
   
   it("Debería cambiar las dimensiones de la imagen a 6x6 ", function() {
