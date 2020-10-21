5aa0a79c7bec204eca579301bc89acad5c4fc84f
Alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-10-21 18:14:15 +0200

    Actualizando test

--- test/testImagen.js
+++ test/testImagen.js
@@ -7,19 +7,22 @@ var assert = require('chai').assert;
 describe("IMAGEN CLASS TEST", function() { 
   let imagen = new Imagen(3,3);
 
-  it("Debería haber sido creada una imagen de 3x3 con sus valores de pixel a 0", function() {
-    assert.notEqual(3, 4, 'these numbers are not equal');
+    context('1. Constructor', function(){
+        
+        it("1.1 Debería haber sido creada una imagen de 3x3 con sus valores de pixel a 0", function() {
+            assert.equal(imagen.getFilas, 3, "Debería ser 3");
+            assert.equal(imagen.getColumnas, 3, "Debería ser 3");
+        });
+    });
 
-    assert.equal(imagen.getFilas, 3, "Debería ser 3");
-    assert.equal(imagen.getColumnas, 3, "Debería ser 3");
-  });
-  
-  it("Debería cambiar las dimensiones de la imagen a 6x6 ", function() {
-    imagen.setFilas = 6;
-    imagen.setColumnas = 6;
+    context('2. Setter', function(){
+        it("2.1 Debería cambiar las dimensiones de la imagen a 6x6 ", function() {
+            imagen.setFilas = 6;
+            imagen.setColumnas = 6;
 
-    assert.notStrictEqual(imagen.columnas, 6, "El número de columnas no se ha actualizado");
-    assert.notStrictEqual(imagen.filas, 6, "El número de filas no se ha actualizado");
-  });
+            assert.notStrictEqual(imagen.columnas, 6, "El número de columnas no se ha actualizado");
+            assert.notStrictEqual(imagen.filas, 6, "El número de filas no se ha actualizado");
+        });
+    });
 
 });
--- test/testRaw.js
+++ test/testRaw.js
@@ -5,9 +5,11 @@ var assert = require('chai').assert;
 describe("RAW CLASS TEST", function() { 
   let raw = new Raw(3,3);
 
-  it("Debería haber sido creado un objeto raw de 3x3", function() {
-    assert.equal(raw.getRaw.byteLength, 9, "Debería ser 9");
-   
-  });
+    context('1. Constructor', function(){
+
+        it("1.1 Debería haber sido creado un objeto raw de 3x3", function() {
+            assert.equal(raw.getRaw.byteLength, 9, "Debería ser 9");
+        });
+    });
 
 });
