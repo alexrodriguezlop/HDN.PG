d2801f6dcb4e15bf0ab07388abd9a3f1d1a97e0d
Alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-10-21 17:57:08 +0200

    Se corrigen fallos en los test y se dividen en varios ficheros close #32

--- test/test.js
+++ test/test.js
@@ -1,22 +1,3 @@
-const Imagen = require('../src/class/imagen')
-var assert = require('chai').assert;
+require("./testRaw");
 
-
-describe("IMAGEN CLASS TEST", function() { 
-  let imagen = new Imagen(3,3);
-
-  it("Debería crear una imagen de 3x3 con sus valores de pixel a 0", function() {
-    assert.notEqual(3, 4, 'these numbers are not equal');
-
-    assert.equal(imagen.getFilas, 3, "Debería ser 3");
-    assert.equal(imagen.getColumnas, 3, "Debería ser 3");
-  });
-  
-  it("Debería cambiar las dimensiones de la imagen a 6x6 ", function() {
-    imagen.setFilas = 6;
-    imagen.setColumnas = 6;
-
-    assert.notStrictEqual(imagen.columnas, 6, "El número de columnas no se ha actualizado");
-    assert.notStrictEqual(imagen.filas, 6, "El número de filas no se ha actualizado");
-  });
-});
+require("./testImagen");
