f5b9488418fe589817dcba99db45e4fb319240e9
Alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-10-26 13:49:49 +0100

    Añadida tarea test para ejecutar los test de mocha

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,30 @@
-module.exports = function(grunt){
 
-    //Objeto de configuración
-    grunt.initConfig({
-        // Cargar info del paquete
-        pkg: grunt.file.readJSON('package.json')
-    });
+var gulp = require('gulp');
+const { series } = require('gulp');
+const mocha = require('gulp-mocha');
 
-    function defaultTask(cb) {
-        // Tareas previas a la llamada a función
-        cb();
-    }
+gulp.task('test', done => {
+    gulp
+        .src('./test/test.js')
+        .pipe(mocha())
+        .on('error', function () {
+            this.emit('end');   
+        })
+        ;done();
+});
 
-    function test(cb) {
-        cb();
-
-    }
-
-    function clean(cb) {
-        cb();
-    }
-
-    function build(cb) {
-        cb();
-    }
-
-    function install(cb) {
+function clean(cb) {
     cb();
-    }
-};
+}
+
+function build(cb) {
+    cb();
+}
+
+function install(cb) {
+cb();
+}
+
 // Define que tarea será la tarea por defecto o agrupaciones bajo etiquetas
-exports.default = defaultTask
+//exports.default = defaultTask
 exports.default = series(clean, build);
