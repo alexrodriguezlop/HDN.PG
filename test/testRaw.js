e2c9b054752edfe250ba0c23b02abbef345b7dc4
alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-12-15 08:42:55 +0100

    Completando testeo de clase Raw para satisfacer HU1 #10 y HU3 #12  [skip ci]

--- test/testRaw.js
+++ test/testRaw.js
@@ -1,14 +1,11 @@
 const Raw = require('../src/class/raw');
 var assert = require('chai').assert;
+const fs = require('fs')
+const fu = require('../src/class/funciones')
 
 describe("RAW CLASS TEST", function() { 
-  let raw = new Raw(3,3);
-
-    context('1. Constructor', function(){
-        it("1.1 Debería haber sido creado un objeto raw de 3x3", function() {
-            assert.equal(raw.getRaw.byteLength, 9, "Debería ser 9");
-        });
-    });
+    let file = fs.readFileSync('./Img/3.pgm');
+    let raw = new Raw(file);
 
     context('2. setPixel', function(){
         raw.setPixel(0b01, 1);
