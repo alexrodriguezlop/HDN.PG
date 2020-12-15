657dbf3e45a3afa199e65742a66456f909dcec7d
alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-12-15 08:39:36 +0100

    Testeo de tratamiento de imágenes desde un fichero [skip ci]

--- test/testImagen.js
+++ test/testImagen.js
@@ -1,26 +1,13 @@
 const Imagen = require('../src/class/imagen');
 var assert = require('chai').assert;
+const fu = require('../src/class/funciones')
+const fs = require('fs')
+
 
 describe("IMAGEN CLASS TEST", function() { 
-  let imagen = new Imagen(25,25);
-
-    context('1. Constructor', function(){
-        
-        it("1.1 Debería haber sido creada una imagen de 25x25 con sus valores de pixel a 0", function() {
-            assert.equal(imagen.getFilas, 25, "Debería ser 25");
-            assert.equal(imagen.getColumnas, 25, "Debería ser 25");
-        });
-    });
-
-    context('2. Setter', function(){
-        it("2.1 Debería cambiar las dimensiones de la imagen a 25x25 ", function() {
-            imagen.setFilas = 30;
-            imagen.setColumnas = 30;
-
-            assert.equal(imagen.getColumnas, 30, "El número de columnas no se ha actualizado");
-            assert.equal(imagen.getFilas, 30, "El número de filas no se ha actualizado");
-        });
-    });
+    let file = fs.readFileSync('./Img/3.pgm');
+    let metadatos = fu.leerMeta(file);
+    let imagen = new Imagen(file, metadatos[0], metadatos[1], metadatos[2], metadatos[3], metadatos[4]);
 
     context('14. Ocultar', function(){
         it("14.1 Debería OCULTAR el mensaje en una imagen", function() {
@@ -31,10 +18,12 @@ describe("IMAGEN CLASS TEST", function() {
 
     context('16. Revelar', function(){
         it("16.1 Debería REVELAR el mensaje oculto en una imagen", function() {
-            var mensaje = imagen.revelar(mensaje);
-            //console.log("Mensaje " + mensaje);
-            assert.isString(imagen.revelar(mensaje), "Deberia devolver un String");
+            var revelado = imagen.revelar();
+            console.log(revelado);
+            assert.isString(revelado, "Deberia devolver un string");
         });
     });
+
+    imagen.escribirImagen("Prueba.pgm");
 
 });
