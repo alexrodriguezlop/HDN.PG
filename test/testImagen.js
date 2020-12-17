6fe78af5bcb5d198b03cdd17add6f2fc80f059db
alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-12-17 09:54:38 +0100

    Apliados test de Imagen #67 #10 #12 [skip ci]

--- test/testImagen.js
+++ test/testImagen.js
@@ -1,29 +1,47 @@
 const Imagen = require('../src/class/imagen');
+const fu = require('../src/class/funciones');
+const fs = require('fs');
 var assert = require('chai').assert;
-const fu = require('../src/class/funciones')
-const fs = require('fs')
+var expect = require('chai').expect;
 
 
-describe("IMAGEN CLASS TEST", function() { 
-    let file = fs.readFileSync('./Img/3.pgm');
+
+
+describe("IMAGEN CLASS TEST", function() {
+    // Carga una imágen 
+    let file = fs.readFileSync('./Img/Ejemplo.pgm');
     let metadatos = fu.leerMeta(file);
     let imagen = new Imagen(file, metadatos[0], metadatos[1], metadatos[2], metadatos[3], metadatos[4]);
 
+    context('5. getMaxTam', function(){
+        it("5.1 Debería calcular el máximo número de caractéres que cabe en una imágen", function() {
+            var maxTam = imagen.getMaxTam();
+
+            expect(maxTam).to.be.a('number');
+            expect(maxTam % 1).to.equal(0);
+        });
+    });
+
     context('14. Ocultar', function(){
         it("14.1 Debería OCULTAR el mensaje en una imagen", function() {
-            var mensaje = "Hola";
+            var mensaje = "!ADIOS MUNDO 20202!";
+            console.log("        Mensaje: " + mensaje);
+            
             assert.isTrue(imagen.ocultar(mensaje), "Deberia devolver True");
+
+            imagen.escribirImagen("./Img/oculto.pgm");
         });
     });
 
     context('16. Revelar', function(){
         it("16.1 Debería REVELAR el mensaje oculto en una imagen", function() {
-            var revelado = imagen.revelar();
-            console.log(revelado);
-            assert.isString(revelado, "Deberia devolver un string");
+            let fileMensaje = fs.readFileSync('./Img/oculto.pgm');
+            let meta = fu.leerMeta(fileMensaje);
+            let imagenMensaje = new Imagen(fileMensaje, meta[0], meta[1], meta[2], meta[3], meta[4]);
+        
+            var revelado = imagenMensaje.revelar();
+            console.log("        Mensaje: " + revelado);
+            assert.isString(revelado, "Deberia devolver un string (Hola)");
         });
     });
-
-    imagen.escribirImagen("Prueba.pgm");
-
 });
