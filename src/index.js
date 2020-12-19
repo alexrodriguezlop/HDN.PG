4e4367c57c89313404249c99fed9335313ee80a4
alex Rodríguez <alexrodriguezlop@correo.ugr.es>
2020-12-19 11:43:41 +0100

    Mejorada interfaz api   [skip ci]

--- src/index.js
+++ src/index.js
@@ -13,40 +13,85 @@ server.use(restify.plugins.bodyParser());
 */
 
 server.post('/ocultar',(req, res, next) =>{
-  var path = req.files[''].path;
-  var nombre = req.files[''].name;
-  var msg = req.body.msg;
 
-  // Oculto el mensaje
-  var buffer = iC.ocultar(path,nombre, msg);
+  if(req.files != undefined && req.files[''] != undefined && req.body.msg != undefined){
+    var path = req.files[''].path;
+    var nombre = req.files[''].name;
+    var msg = req.body.msg;
 
-  res.status(200);
-  res.header('content-type', 'img/pgm');
-  res.send(buffer);
+    // Oculto el mensaje
+    var buffer = iC.ocultar(path,nombre, msg);
 
+    res.status(200);
+    res.header('content-type', 'img/pgm');
+    res.send(buffer);
+  }
+  else{
+    res.status(400);
+    res.send();
+  }
   next();
 });
 
 
 server.post('/revelar',(req, res, next) =>{
-  var path = req.files[''].path;
-  var msg = iC.revelar(path);
-  
-  res.status(200);
-  res.setHeader("Content-Type","application/json");
-  res.send(msg);
 
+  if(req.files != undefined && req.files[''] != undefined){
+    var path = req.files[''].path;
+
+    var msg = iC.revelar(path);
+    
+    res.status(200);
+    res.setHeader("Content-Type","application/json");
+    res.send(msg);
+  }
+  else{
+    res.status(400);
+    res.send();
+  }
   next();
 });
 
 
 server.post('/chequear', (req, res, next) =>{
-  var path = req.files[''].path;
-  var chequeo = iC.chequear(path);
 
-  res.status(200);
-  res.setHeader("Content-Type","application/json");
-  res.send(chequeo);
+  if(req.files != undefined && req.files[''] != undefined){
+    var path = req.files[''].path;
+
+    var chequeo = iC.chequear(path);
+
+    res.status(200);
+    res.setHeader("Content-Type","application/json");
+    res.send(chequeo);
+  }
+  else{
+    res.status(400);
+    res.send();
+  }  
+  next();
+});
+
+server.post('/metadatos', (req, res, next) =>{
+
+  if(req.files != undefined && req.files[''] != undefined){
+    var path = req.files[''].path;
+
+    var metadatos = iC.getMetadatos(path);
+
+    if(metadatos != undefined){
+      res.status(200);
+      res.setHeader("Content-Type","application/json");
+      res.send(metadatos);
+    }
+    else{
+      res.status(404);
+      res.send();
+    }
+  }
+  else{
+    res.status(400);
+    res.send();
+  }  
   next();
 });
 
